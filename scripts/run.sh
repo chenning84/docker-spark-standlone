060d1a326ed4a86943a2efedc968d4fe3e2b46f5
Ning chen <ning.chen1@one.verizon.com>
2016-01-15 16:09:29 -0500

    add local key to docker so that ssh suppress password

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,7 +13,10 @@ fi
 owner=${SPARK_OWNER}
 chown -R "$owner":"$owner" /spark-1.5.2-bin-hadoop2.6
 chown -R "$owner":"$owner" /spark_config
-
+#cp /ssh_config/id_rsa /home/"$owner"/.ssh/id_rsa
+#cp /ssh_config/id_rsa.pub /home/"$owner"/.ssh/id_rsa.pub
+cp /ssh_config/id_rsa.pub /home/"$owner"/.ssh/authorized_keys
+chown -R "$owner":"$owner"  /home/"$owner"/.ssh
 service ssh restart && bash
 
 #exec su - spark
