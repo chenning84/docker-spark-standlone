2961eab4e27d69c59e8b89a6585160c6628a537e
Ning chen <ning.chen1@one.verizon.com>
2016-01-15 17:51:36 -0500

    moving along cp known host to docker

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,10 +13,16 @@ fi
 owner=${SPARK_OWNER}
 chown -R "$owner":"$owner" /spark-1.5.2-bin-hadoop2.6
 chown -R "$owner":"$owner" /spark_config
+
 #cp /ssh_config/id_rsa /home/"$owner"/.ssh/id_rsa
 #cp /ssh_config/id_rsa.pub /home/"$owner"/.ssh/id_rsa.pub
 cp /ssh_config/id_rsa.pub /home/"$owner"/.ssh/authorized_keys
+cp /ssh_config/known_hosts /home/"$owner"/.ssh/known_hosts
+
+
 chown -R "$owner":"$owner"  /home/"$owner"/.ssh
+su - $owner -c 'ssh-keygen -f .ssh/id_rsa -t rsa -N ""'
+su - $owner -c 'cat .ssh/id_rsa.pub >> .ssh/authorized_keys'
 service ssh restart && bash
 
 #exec su - spark
